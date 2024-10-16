bd11bab4e8985f61fc7cf55f3778ed0ef1768d65
Sam <omni@nwrk.co.uk>
2024-10-16 11:10:25 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Pull the latest Docker image for the consensus nodes
+docker pull nocoin/nocoin_node:latest
+
+# Launch the network using Docker Compose
+docker-compose up -d
+
+# Initialize network settings
+docker exec nocoin_node_1 ./initialize_network.sh
