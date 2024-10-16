fe4dd83a6e4fb20902c2db3a28ca4acccd59386f
Sam <omni@nwrk.co.uk>
2024-10-16 12:29:47 +0100

    Update transactions.rs

--- src/transactions/transactions.rs
+++ src/transactions/transactions.rs
@@ -8,6 +8,12 @@ pub struct Transaction {
     pub timestamp: u64,
 }
 
+pub struct SmartContract {
+    pub contract_code: Vec<u8>,
+    pub owner: Vec<u8>,
+    pub state: Vec<u8>,
+}
+
 pub fn create_transaction(sender: Vec<u8>, receiver: Vec<u8>, amount: u64, private_key: Vec<u8>) -> Transaction {
     let tx = Transaction {
         sender,
@@ -25,4 +31,9 @@ pub fn create_transaction(sender: Vec<u8>, receiver: Vec<u8>, amount: u64, priva
 pub fn verify_transaction(tx: &Transaction, public_key: Vec<u8>) -> bool {
     let serialized_tx = serialize_transaction(tx);
     verify_signature(public_key, &serialized_tx, tx.signature.clone())
+}
+
+pub fn execute_contract(contract: &SmartContract, input_data: Vec<u8>, executor_private_key: Vec<u8>) -> Vec<u8> {
+    // Execute the smart contract securely
+    quantum_vm::execute(contract, input_data, executor_private_key)
 }
