8b2e2c340c2fbfc8e403015dc72c867238d68ded
Sam <omni@nwrk.co.uk>
2024-10-16 11:37:57 +0100

    Update wallet.rs

--- src/wallet/wallet.rs
+++ src/wallet/wallet.rs
@@ -7,17 +7,11 @@ pub struct Wallet {
 
 pub fn create_wallet() -> Wallet {
     let (public_key, private_key) = generate_keypair();
-    Wallet {
-        public_key,
-        private_key,
-    }
+    Wallet { public_key, private_key }
 }
 
 pub fn sign_transaction(wallet: &Wallet, transaction: &Transaction) -> Transaction {
     let serialized_tx = serialize_transaction(transaction);
     let signature = sign_message(wallet.private_key.clone(), &serialized_tx);
-    Transaction {
-        signature,
-        ..transaction.clone()
-    }
+    Transaction { signature, ..transaction.clone() }
 }
