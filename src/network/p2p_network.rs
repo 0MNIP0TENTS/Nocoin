6f368904ac37cd0b7d2169dac7995b5b7e7ae06c
Sam <omni@nwrk.co.uk>
2024-10-16 10:48:07 +0100

    Update p2p_network.rs

--- src/network/p2p_network.rs
+++ src/network/p2p_network.rs
@@ -1,1 +1,18 @@
+// p2p_network.rs
 
+pub fn broadcast_transaction(tx: Transaction) {
+    // Broadcast transaction to all peers in the network
+    let peers = get_peer_list();
+    for peer in peers {
+        send_to_peer(peer, serialize_transaction(&tx));
+    }
+}
+
+pub fn synchronize_blockchain() {
+    // Synchronize the local blockchain with the network
+    let peers = get_peer_list();
+    for peer in peers {
+        let remote_chain = fetch_blockchain_from_peer(peer);
+        merge_chains(remote_chain);
+    }
+}
